4e96c0642dae0ad1b1ee8904f38efe55a3e23843
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2018-02-20 00:43:27 +0100

    updating uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -6,14 +6,9 @@
  * @package dashboard_monitor
  */
 
-namespace Dashboard_Monitor;
-
-use Dashboard_Monitor\Helpers as General_Helpers;
-
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
   exit;
 }
 
-$general_helper = new General_Helpers\General_Helper();
-$general_helper->remove_db_option();
+delete_option( 'dashboard_monitor_keys' );
